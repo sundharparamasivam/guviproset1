f4f2341f3d2bb9662ae98377d14b7dfd25ce03d3
sundharparamasivam <48799633+sundharparamasivam@users.noreply.github.com>
2019-07-19 19:19:14 +0530

    Create repeatele.py

---
+++ repeatele.py
@@ -0,0 +1,23 @@
+su=int(input())
+lis=[]
+l=""
+for i in range(su):
+    a=input()
+    lis.append(a)
+co=0
+cou=0
+li1=[]
+for j in lis:
+    if co==0:
+        l+=j
+        co+=1
+    else:
+        for k in range(10):
+            if j[:(len(j)-cou)] in l:
+                l+=(j[:(len(j)-cou)])
+                li1.append(len(j[:(len(j)-cou)]))
+                break
+            else:
+                cou+=1
+aaa=min(li1)
+print(l[:aaa])
